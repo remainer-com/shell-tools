58e0e5c6f42643f247e3cf2b674d9c1282cbd0b4
remainer <remainer@163.com>
2016-07-20 11:22:45 +0800

    set path; copy

---
+++ windows/copy.bat
@@ -0,0 +1,9 @@
+rem-----------------------------------------------
+rem copy命令用法示例
+rem 语法 copy /y 源文件 目标路径
+rem /y参数：直接覆盖同名文件
+rem 用法较多
+rem-----------------------------------------------
+
+copy /y E:\hosts\hosts C:\Windows\System32\drivers\etc
+pause
