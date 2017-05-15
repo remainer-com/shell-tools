95733b351fce44158a6ad425738c7ad9459b81fb
remainer <remainer@163.com>
2017-05-15 17:49:58 +0800

    自动部署添加注释

--- linux/deploy.sh
+++ linux/deploy.sh
@@ -1,3 +1,16 @@
+# 从svn上更新代码
+svn update
+# PATH环境变量中添加maven
 PATH=$PATH:"/D/apache-maven-3.3.9/bin"
+# 使用maven打包代码
 mvn clean package
-scp -P 10026 -r "target/oto.fsgw-0.0.1-SNAPSHOT.jar" root@123.125.147.106:/home/oto/fsgw/oto.fsgw-0.0.1-SNAPSHOT.jar
+# 删除打包后的代码中和配置无关的，依自身情况自行添加或删除
+rm -rf target/classes/cn/ target/classes/static/ target/classes/META-INF/
+# 上传打包的jar文件
+scp -P 10026 -r "target/oto.vpi-1.1.1.20170112_alpha.jar" root@123.125.147.106:/home/oto/vpi/oto.vpi-1.1.1.20170112_alpha.jar
+# 上传配置文件
+# 我的项目是spring boot的项目，需要添加配置文件到服务器上jar包同级config/下
+scp -P 10026 -r "target/classes/" root@123.125.147.106:/home/oto/vpi/config/
+# 运行start.sh文件
+# start.sh文件内容参照本项目linux/start.sh
+ssh -p 10026 root@123.125.147.106 'cd /home/oto/vpi/ &&./start.sh'
--- linux/start.sh
+++ linux/start.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
+# 加载/etc/profile文件，不加载容易出现commond not found 错误，原因见
+# http://feihu.me/blog/2014/env-problem-when-ssh-executing-command-on-remote/
 source /etc/profile
 
+# 判断run.pid文件是否存在
 if [ ! -f "run.pid" ]
-then
+then # 不存在
 	echo "server is not running, begin start"
-else
-	cat run.pid | if read PID
+else # 存在
+	cat run.pid | if read PID # 读取第一行，第一行存在
 	then
-		kill $PID & echo "stop $PID"
+		kill $PID & echo "stop $PID" # 杀掉进程并显示stop 进程号(pid)
 	fi
-	rm -f run.pid
+	rm -f run.pid # 删除run.pid文件
 fi
+# 运行程序并记录进程号(pid)
+# 若您不是Java项目，只需要编辑为您的运行命令并在之后添加  2>&1 & echo $! > run.pid
 java -Xmx1024M -Xms512M -XX:MaxPermSize=256m -jar ghss-1.1.1.20170228_alpha.jar --spring.profiles.active=prod 2>&1 & echo $! > run.pid
