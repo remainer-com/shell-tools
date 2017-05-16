8267bb1a3566b853cdabf1d6dd689992469f71eb
remainer <remainer@163.com>
2017-05-16 12:27:48 +0800

    修复linux自动部署的一些bug

--- linux/deploy.sh
+++ linux/deploy.sh
@@ -1,3 +1,9 @@
+#! /bin/bash
+
+# 设置变量
+destination=/home/oto/ghss/
+filename=ghss-1.1.1.20170228_alpha.jar
+
 # 从svn上更新代码
 svn update
 # PATH环境变量中添加maven
@@ -7,10 +13,14 @@ mvn clean package
 # 删除打包后的代码中和配置无关的，依自身情况自行添加或删除
 rm -rf target/classes/cn/ target/classes/static/ target/classes/META-INF/
 # 上传打包的jar文件
-scp -P 10026 -r "target/oto.vpi-1.1.1.20170112_alpha.jar" root@123.125.147.106:/home/oto/vpi/oto.vpi-1.1.1.20170112_alpha.jar
+scp -P 10026 -r "target/$filename" root@127.0.0.1:$destination$filename
+# 重命名classes文件夹为config(因没找见scp上传同时修改文件夹名称的方法，望有知道的麻烦告知一下)
+mv -f target/classes/ target/config/
 # 上传配置文件
 # 我的项目是spring boot的项目，需要添加配置文件到服务器上jar包同级config/下
-scp -P 10026 -r "target/classes/" root@123.125.147.106:/home/oto/vpi/config/
+scp -P 10026 -r "target/config/" root@127.0.0.1:$destination
+# 删除config文件夹
+rm -rf target/config/
 # 运行start.sh文件
 # start.sh文件内容参照本项目linux/start.sh
-ssh -p 10026 root@123.125.147.106 'cd /home/oto/vpi/ &&./start.sh'
+ssh -p 10026 root@127.0.0.1 "cd $destination &&./start.sh"
--- linux/start.sh
+++ linux/start.sh
@@ -9,10 +9,13 @@ if [ ! -f "run.pid" ]
 then # 不存在
 	echo "server is not running, begin start"
 else # 存在
-	cat run.pid | if read PID # 读取第一行，第一行存在
-	then
-		kill $PID & echo "stop $PID" # 杀掉进程并显示stop 进程号(pid)
-	fi
+	# cat run.pid | if read PID # 读取第一行，第一行存在
+	# then
+	# 	kill $PID & echo "stop $PID" # 杀掉进程并显示stop 进程号(pid)
+	# fi
+	PID=$(cat run.pid) # 读取pid
+	kill $PID & echo "stop $PID" # 杀掉进程并显示stop 进程号(pid)
+
 	rm -f run.pid # 删除run.pid文件
 fi
 # 运行程序并记录进程号(pid)
